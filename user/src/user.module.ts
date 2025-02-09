58eeb76a4cbf9aeec42d195900e318c9ce293cca
aminvv <aminviper1378@gmail.com>
2025-02-09 13:52:48 +0330

    User related service, part one--------->>>>>>.(chapter:12 section:4)

---
+++ user/src/user.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { UserController } from './user.controller';
+import { UserService } from './user.service';
+
+
+@Module({
+  imports: [MongooseModule.forRoot("mongodb://localhost:27017/microservice-todo")],
+  controllers: [UserController],
+  providers: [UserService],
+})
+export class UserModule {}
